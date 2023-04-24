24ac0dbd1615e7a7379c770995c4a0464104e6fc
Sarah Brightman <sarah.graciela.brightman@gmail.com>
2023-04-24 16:37:38 -0500

    adding contacts, add contacts, search Contacts, and switch

--- contacts/Contacts.java
+++ contacts/Contacts.java
@@ -4,17 +4,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
 
 public class Contacts {
+    Scanner scan = new Scanner(System.in);
     String directory = "contacts";
     String fileName = " contacts.txt";
 
     Path dataDirectory = Paths.get(directory);
     Path dataFile = Paths.get(directory, fileName);
 
-    public  void makeIt(){
+    public  void makeFile(){
 
 
         if(Files.notExists(dataFile)){
@@ -33,19 +34,84 @@ public class Contacts {
         }
 
     }
+    List<String> contactName = Arrays.asList("Oggie Booggie | 800-243-1025", "Your Mom | 420-666-6969", "Sponge Bob | 915-243-6754");
+
     public void contacts(){
 
-        List<String> contactName = Arrays.asList("Oggie Booggie | 800-243-1025", "Your Mom | 420-666-6969", "Kim K | 915-243-6754");
-        System.out.println(contactName);
+//        System.out.println(contactName);
+        System.out.println("name | phone number");
+        System.out.println("-----------------------");
+
         try {
             List<String > contactNameFiles = Files.readAllLines(dataFile);
 
-            for(int i =  1; i<= contactNameFiles.size(); i +=1){
-                System.out.println(i+": "+ contactNameFiles.get(i-1));
+            for (String contact : contactNameFiles){
+                System.out.println(contact);
+                System.out.println("-----------------------");
             }
         }catch (IOException e){
             e.printStackTrace();
         }
     }
+    public void addContacts(){
 
+        String addedCon = scan.nextLine();
+        boolean hasIt = false;
+        try{
+            List<String> lines = Files.readAllLines(dataFile);
+            for(String line: lines){
+                if(line.equals(addedCon)){
+                    hasIt = true;
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        if(!hasIt) {
+
+            try {
+                List<String> currentList = Files.readAllLines(dataFile);
+                currentList.add(addedCon);
+                Files.write(dataFile, currentList);
+                contacts();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+//        System.out.println(contactName);
+
+}
+public void searchContact(){
+    System.out.println("enter name");
+
+    String searchContact = scan.nextLine();
+
+    try{
+        List<String> lines = Files.readAllLines(dataFile);
+        for(String line: lines){
+            if(line.contains(searchContact)){
+                System.out.println(line);
+            }
+        }
+    }catch (IOException e){
+        e.printStackTrace();
+    }
+}
+public void deleteContact(){
+    System.out.println("enter name");
+
+    String searchContact = scan.nextLine();
+    try{
+        List<String> removelines = Files.readAllLines(dataFile);
+        for(String line: removelines){
+            if(line.equalsIgnoreCase(searchContact)){
+
+                removelines.remove(searchContact);
+            }
+        }
+    }catch (IOException e){
+        e.printStackTrace();
+    }
+}
 }
